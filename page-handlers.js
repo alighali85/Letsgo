f49e1ce1f766c6c5c62e39cc1cc0b127262eafe5
Ali Ghali <ali.ghali@sap.com>
2017-08-15 23:54:18 +0200

    add new project slide

--- page-handlers.js
+++ page-handlers.js
@@ -1,23 +1,49 @@
-function myFunction() {
-    var x = document.getElementById('myDIV');
-    x.style.display = 'none';
-}
+$(document).ready(function () {
 
-function onload() {
-
-    if (window.jQuery) {
-        // jQuery is loaded  
-    } else {
-        // jQuery is not loaded
-        alert("you are not connected to the internet");
+    function myFunction() {
+        var x = document.getElementById('myDIV');
+        x.style.display = 'none';
     }
-};
 
-$(window).on("scroll", function () {
-    if ($(this).scrollTop() > 100) {
-        $(".header-container").css("background", "#2A2B49");
-        $(".header-container").css("z-index", "9999");
-    } else {
-        $(".header-container").css("background", "none");
-    }
-});
+    function onload() {
+
+        if (window.jQuery) {
+            // jQuery is loaded  
+        } else {
+            // jQuery is not loaded
+            alert("you are not connected to the internet");
+        }
+    };
+
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 100) {
+            $(".header-container").css("background", "#2A2B49");
+            $(".header-container").css("z-index", "9999");
+        } else {
+            $(".header-container").css("background", "none");
+        }
+        if ($(this).scrollBottom() < 100) {
+            $(".header-container").css("background", "red");
+
+        }
+    });
+
+    $(".card").click(function () {
+        var slide = $('.card-no')[$('.card').index(this)];
+        if ($(slide).css('visibility') === 'hidden') {
+            $(slide).css('visibility', 'visible');
+            // $('.card').css('pointer-events', 'none');
+        } else {
+            $(slide).css('visibility', 'hidden');
+        };
+    });
+
+    $(".card-x").click(function () {
+        alert('card x');
+        $(slide).css('visibility', 'hidden');
+    });
+
+    $('.new-project').click(function () {
+        $()
+    })
+})
